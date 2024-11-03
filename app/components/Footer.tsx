cde0d1508ed9fc7b0bf8845dce94fd74db4d07df
Muhammad Nofil Shoaib <164288852+MuhammadNofilShoaib@users.noreply.github.com>
2024-11-03 01:45:24 -0800

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
     <div className='footer'>
         <div className='heading'><p>connect❤️</p></div>
       <div className='images'>
-        <Image className='img' src='/gh.png' alt='github' width={200} height={200}></Image>
-        <Image className='img' src='/yt.png' alt='github' width={200} height={200}></Image>
-        <Image className='img' src='/ig.png' alt='github' width={200} height={200}></Image>
-        <Image className='img' src='/li.png' alt='github' width={200} height={200}></Image>
+        <a href="https://www.linkedin.com/in/nofil-shoaib-6b284328a" target='_blank'><Image className='img' src='/li.png' alt='linkedin' width={200} height={200}></Image></a>
+     <a href="https://www.youtube.com/@Itsgamingwarden" target='_blank'><Image className='img' src='/yt.png' alt='youtube' width={200} height={200}></Image></a>
+     <a href="https://github.com/MuhammadNofilShoaib" target='_blank'><Image className='img' src='/gh.png' alt='github' width={200} height={200}></Image></a>
+     <a href="https://www.instagram.com/muhammad_nofil_shoaib/" target='_blank'><Image className='img' src='/ig.png' alt='instagram' width={200} height={200}></Image></a>
       </div>
       <div className="mail">
         <a className='mail' href="mailto:nofilshoaib@gmail.com">nofilshoaib@gmail.com</a>
